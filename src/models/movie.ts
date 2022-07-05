01b4c1d7e7f8fe84d6dc5287bb490b889dee7199
Albert Figueras <afigueras21@me.com>
2022-07-05 17:04:18 +0200

    add movie interface

---
+++ src/models/movie.ts
@@ -0,0 +1,10 @@
+export default interface Movie {
+    id:number,
+    title: string,
+    duration: string,
+    genres: string[],
+    director: string,
+    actors: string,
+    plot: string,
+    posterUrl: string,
+}
