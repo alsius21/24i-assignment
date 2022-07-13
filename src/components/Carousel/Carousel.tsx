9ec32e9be81530f7415c6da5eec816eabc3ee7f3
Albert Figueras <afigueras21@me.com>
2022-07-13 22:03:02 +0200

    add styles

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { fonts, spacing, themes } from '../../utils/styles';
 
 type Item = { title: string; assetUrl: string; id: number };
 type Props = {
@@ -12,6 +13,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
+    paddingBottom: spacing.padding_bottom,
   },
   itemsContainer: {
     flex: 1,
@@ -20,19 +22,30 @@ const styles = StyleSheet.create({
   item: {
     flex: 1,
     flexDirection: 'column',
-    paddingRight: '5',
-    paddingBottom: '5',
+    marginBottom: spacing.padding_bottom,
+    paddingRight: spacing.padding_right,
+    paddingBottom: spacing.padding_bottom,
+  },
+  listTitle: {
+    fontSize: fonts.h2,
+    fontWeight: 'bold',
+    color: themes.dark.base_highlight_color,
+  },
+  movieTitle: {
+    fontSize: fonts.h4,
+    color: themes.dark.base_contrast_color,
   },
   cover: {
     width: 300,
     height: 300,
+    paddingBottom: spacing.padding_bottom,
   },
 });
 
 export default function Carousel({ title, items, onClickCover }: Props) {
   return (
     <View style={styles.container}>
-      <Text>{title}</Text>
+      <Text style={styles.listTitle}>{title}</Text>
       <ScrollView style={styles.itemsContainer} horizontal>
         {items.map((it: Item) => (
           <TouchableOpacity
@@ -46,7 +59,7 @@ export default function Carousel({ title, items, onClickCover }: Props) {
               }}
               style={styles.cover}
             />
-            <Text>{it.title}</Text>
+            <Text style={styles.movieTitle}>{it.title}</Text>
           </TouchableOpacity>
         ))}
       </ScrollView>
---
+++ src/utils/styles.ts
@@ -0,0 +1,35 @@
+const SIZE_XXS = 2.5;
+const SIZE_XS = 5;
+const SIZE_S = 7.5;
+const SIZE_M = 10;
+const SIZE_L = 15;
+const SIZE_XL = 30;
+export const BASE_COLOR = '#003941';
+export const BASE_LEFT_PADDING = SIZE_M;
+export const BASE_RIGHT_PADDING = SIZE_M;
+export const BASE_BOTTOM_PADDING = SIZE_L;
+export const BASE_CONTRAST_COLOR = 'white';
+export const BASE_HIGHLIGHT_COLOR = '#fcc';
+
+export const spacing = {
+  padding_left: BASE_LEFT_PADDING,
+  padding_right: BASE_RIGHT_PADDING,
+  padding_bottom: BASE_BOTTOM_PADDING,
+};
+
+export const themes = {
+  dark: {
+    base_background_color: BASE_COLOR,
+    base_contrast_color: BASE_CONTRAST_COLOR,
+    base_highlight_color: BASE_HIGHLIGHT_COLOR,
+  },
+};
+
+export const fonts = {
+  h1: 24,
+  h2: 20,
+  h3: 18,
+  h4: 16,
+  h5: 14,
+  text: 12,
+};
