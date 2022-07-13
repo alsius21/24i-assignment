509c7c10479d41f34492e0b5d508fea34def8c75
Albert Figueras <afigueras21@me.com>
2022-07-13 21:18:29 +0200

    add carousel styles

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,32 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 type Item = { title: string; assetUrl: string; id: number };
 type Props = {
   title: string;
   items: Item[];
+  onClickCover(id: number): void;
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
   },
+  itemsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  item: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingRight: '5',
+    paddingBottom: '5',
+  },
+  cover: {
+    width: 300,
+    height: 300,
+  },
 });
 
-export default function Carousel({ title, items }: Props) {
+export default function Carousel({ title, items, onClickCover }: Props) {
   return (
     <View style={styles.container}>
       <Text>{title}</Text>
-      {items.map((it: Item) => (
-        <View key={it.id}>
-          <Image
-            source={{
-              uri: it.assetUrl,
-            }}
-          />
-          <Text>{it.title}</Text>
-        </View>
-      ))}
+      <ScrollView style={styles.itemsContainer} horizontal>
+        {items.map((it: Item) => (
+          <TouchableOpacity
+            key={it.id}
+            style={styles.item}
+            onPress={() => onClickCover(it.id)}
+          >
+            <Image
+              source={{
+                uri: it.assetUrl,
+              }}
+              style={styles.cover}
+            />
+            <Text>{it.title}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 }
